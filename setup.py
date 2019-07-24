485332bc45bedd05f76e01da38fa2e40f794eb3a
Tom Stanley <tstanleyuk@icloud.com>
2019-07-24 15:39:10 +0800

    change to MIT license

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requirements_array = build_requirements_array()
 
 setuptools.setup(
     name="WorldTradingDataPythonSDK",
-    version="0.0.1",
+    version="0.0.2",
     author="Tom Stanley",
     author_email="tstanleyuk@icloud.com",
     description="Python SDK to interact with www.worldtradingdata.com",
